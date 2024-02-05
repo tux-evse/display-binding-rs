c34455295c7c714d975b3d1b68f4d74f202612d3
Ronan Le Martret <ronan.lemartret@iot.bzh>
2024-02-05 14:59:27 +0100

    Update slac-types.rs
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- afb-types/update-type-rs.sh
+++ afb-types/update-type-rs.sh
@@ -4,4 +4,4 @@ cp ../../ti-am62x-binding-rs/afb-types/src/am62x-types.rs ./src/am62x-types.rs
 cp ../../auth-binding-rs/afb-types/src/auth-types.rs      ./src/auth-types.rs
 cp ../../charging-binding-rs/afb-types/src/chmgr-types.rs ./src/chmgr-types.rs
 cp ../../energy-binding-rs/afb-types/src/engy-types.rs    ./src/engy-types.rs
-cp ../../slac-binding-rs/afb-types/src/am62x-types.rs     ./src/slac-types.rs
+cp ../../slac-binding-rs/afb-types/src/slac-types.rs     ./src/slac-types.rs
