2d276d06d61cb632530468d395d1d7bb7d1b0b8c
Fulup Ar Foll <fulup@iot.bzh>
2024-02-27 17:16:59 +0100

    update engy type

--- afb-types/src/engy-types.rs
+++ afb-types/src/engy-types.rs
@@ -13,12 +13,14 @@ use afbv4::prelude::*;
 use serde::{Deserialize, Serialize};
 use  std::time::Duration;
 
+AfbDataConverter!(meter_tag_set, MeterTagSet);
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub enum MeterTagSet {
     Current,
     Tension,
     Power,
     OverCurrent,
+    AvailCurrent,
     Energy,
     #[default]
     Unset,
@@ -50,9 +52,15 @@ pub struct MeterDataSet {
 
 impl MeterDataSet {
     pub fn default(tag: MeterTagSet) -> Self {
+        let variation= match tag {
+            MeterTagSet::AvailCurrent => 0,
+            MeterTagSet::OverCurrent => 0,
+            _ => 1,
+        };
+
         MeterDataSet {
             tag: tag,
-            variation: 1,
+            variation: variation,
             start: 0,
             updated: false,
             total: 0,
@@ -69,14 +77,14 @@ impl MeterDataSet {
         match phase {
             0 => {
                 let value = value - self.start;
-                if self.total - variation < value || self.total + variation > value 
+                if self.total - variation < value || self.total + variation > value
                 {
                     self.total = value;
                     self.updated = true;
                 }
             }
             1 => {
-                if self.l1 - variation < value || self.l1 + variation > value 
+                if self.l1 - variation < value || self.l1 + variation > value
                 {
                     self.l1 = value;
                 }
@@ -96,7 +104,7 @@ impl MeterDataSet {
             _ => return afb_error!("data-set-update", "invalid phase:{}", phase),
         }
         Ok(())
-    }
+}
 }
 
 AfbDataConverter!(energy_actions, EnergyAction);
@@ -114,32 +122,33 @@ pub enum EnergyAction {
 AfbDataConverter!(energy_state, EnergyState);
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct EnergyState {
+    // private data
     #[serde(skip)]
     pub subscription_max: i32,
     #[serde(skip)]
-    pub tension_max: i32,
+    pub umax: i32,
     #[serde(skip)]
     pub imax: i32,
     #[serde(skip)]
     pub pmax: i32,
+    pub volts: i32,
     // public data
     pub timestamp: Duration,
     pub session: i32,
-    pub total: i32,
     pub current: i32,
     pub tension: i32,
     pub power: i32,
 }
 
 impl EnergyState {
-    pub fn default() -> Self {
+    pub fn default(imax: i32, pmax: i32,umax: i32) -> Self {
         EnergyState {
             subscription_max: 0,
-            imax: 0,
-            pmax: 0,
-            tension_max: 0,
+            imax: imax*100,
+            pmax: pmax*100,
+            umax: umax*100,
             session: 0,
-            total: 0,
+            volts: 0,
             current: 0,
             tension: 0,
             power: 0,
@@ -153,5 +162,6 @@ pub fn engy_registers() -> Result<(), AfbError> {
     config_data_set::register()?;
     energy_actions::register()?;
     energy_state::register()?;
+    meter_tag_set::register()?;
     Ok(())
 }
